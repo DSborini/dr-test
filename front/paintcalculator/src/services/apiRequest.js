ae1f490d4665a9e280e409c00a26cd1b54026dec
Daniel <dsouzaborini@gmail.com>
2021-11-04 22:55:31 -0300

    fix req file

--- front/paintcalculator/src/services/apiRequest.js
+++ front/paintcalculator/src/services/apiRequest.js
@@ -1,13 +1,20 @@
 const axios = require('axios');
 
-const validateWallsInfo = async (wallsInfo) => {
-  const resp = await axios({
-    method: 'get',
-    url: 'http://localhost:3001/paintings/',
-    data: wallsInfo,
-  });
+const api = axios.create({
+  baseURL: 'http://localhost:3001',
+  timeout: 10000,
+});
 
-  return resp;
+const validateWallsInfo = async (wallsInfo) => {
+  api.get('/paintings/', { data: wallsInfo } )
+  .then(function (response) {
+    console.log(response.status);
+    return response.data;
+  })
+  .catch(function (error) {
+    console.log(error.response.data);
+    return error.response.data;
+  });
 };
 
 export default validateWallsInfo;
